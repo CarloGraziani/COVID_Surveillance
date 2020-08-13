65e6a588ef294d3d7174eadc4897ce3b76cc1828
Marieme Ngom <mngom@MacBook-Pro.local>
2020-08-13 15:15:39 -0500

    Updates

--- get_data_simulation.m
+++ get_data_simulation.m
@@ -3,10 +3,10 @@ filename = 'data/simulation_data.txt';
 [A,delimiterOut]=importdata(filename)
 M = A.data;
 [nn,mm] = size(M)
-N_days = 0:1:(nn-2);
+N_days = 10:1:(nn+10-1);
 N_days = N_days';
-N_xt = M(2:end,1);
-C_xt = M(2:end,2);
+N_xt = M(1:end,1);
+C_xt = M(1:end,2);
 data_simulation = [N_days, N_xt, C_xt];
 data_simulation = data_simulation';
 save('data/test_data_simulation','data_simulation')
